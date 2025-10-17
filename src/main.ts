911ff181149c4b609c3484a9903368090619452a
gustavosrosa <g.dsantosrosa@gmail.com>
2025-10-17 15:05:11 -0300

    FEAT: Limpeza do projeto executada

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Add the necessary CSS
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
 createApp(App).use(router).mount('#app')
