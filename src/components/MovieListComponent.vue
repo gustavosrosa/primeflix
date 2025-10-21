a72ad881aa4570d4e8e06b1e08e0f5e701dd2285
gustavosrosa <g.dsantosrosa@gmail.com>
2025-10-21 16:28:01 -0300

    FEAT: Logica de exclusao de itens selecionados

--- src/components/MovieListComponent.vue
+++ src/components/MovieListComponent.vue
@@ -12,7 +12,7 @@
                 <BTd>{{ movie.id }}</BTd>
                 <BTd>{{ movie.title }}</BTd>
                 <BTd>
-                    <BFormCheckbox :id="movie.id" v-model="selectedItems" name="checkbox-1" :value="movie.id">
+                    <BFormCheckbox :id="movie.id" v-model="selectedItems" name="checkbox-1" :value="movie.id" @update:model-value="checkboxFunc()">
                     </BFormCheckbox>
                 </BTd>
             </BTr>
@@ -23,10 +23,15 @@
 <script setup>
 
 import { BTableSimple, BTbody, BTr, BTd, BThead, BTh, BFormCheckbox } from 'bootstrap-vue-next';
-import { defineProps, ref } from 'vue';
+import { defineProps, defineEmits, ref } from 'vue';
 
 const props = defineProps(['movieList']);
-const selectedItems = ref([])
+const selectedItems = ref([]);
+const emit = defineEmits(['movies']);
+
+function checkboxFunc() {
+    emit('movies', selectedItems.value);
+}
 
 </script>
 
--- src/views/MyMoviesView.vue
+++ src/views/MyMoviesView.vue
@@ -2,8 +2,8 @@
     <div class="m-4 ms-5">
         <h1 class="mb-5" >Lista dos Filmes</h1>
         <div class="container">
-            <MovieListComponent :movieList="movieList"/>
-            <BButton class="mt-5" variant="danger" :disabled="true">Remover filmes</BButton>
+            <MovieListComponent @movies="selectedMovies" :movieList="movieList"/>
+            <BButton class="mt-5" variant="danger" :disabled="moviesToDelete.length == 0">Remover filmes</BButton>
         </div>
     </div>
 </template>
@@ -22,13 +22,18 @@
 <script setup>
 import MovieListComponent from '@/components/MovieListComponent.vue';
 import { getMovieStorage } from '@/services/movie-list.service';
-import { onBeforeMount, reactive } from 'vue';
+import { onBeforeMount, reactive, ref } from 'vue';
 import { BButton } from 'bootstrap-vue-next'
 
 let movieList = reactive([]);
+let moviesToDelete = ref([])
 
 onBeforeMount(() => {
     movieList = getMovieStorage();
 })
+
+function selectedMovies(movies) {
+    moviesToDelete.value = movies;
+}
 
 </script>
